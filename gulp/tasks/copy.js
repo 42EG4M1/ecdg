bb5f505426e1ff89bcba798515effb30e32f9401
tatsuya azegami <theorthodoxworks@gmail.com>
2016-07-17 19:54:35 +0900

    Update gulp（更新ファイルのみコピー）

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,10 +1,12 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const config      = require('../config').copy;
+const $           = require('gulp-load-plugins')();
 
 
 gulp.task('copy', () => {
   return gulp.src(config.src)
+  .pipe($.changed(config.dest))
   .pipe(gulp.dest(config.dest))
   .pipe(browserSync.reload({
     stream: true,
