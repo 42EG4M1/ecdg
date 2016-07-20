65d197e1793a15389cc0e54518ee8d305b0f153a
tatsuya azegami <theorthodoxworks@gmail.com>
2016-07-20 18:12:46 +0900

    twitterカードを入れて外部ファイル化

--- src/header.php
+++ src/header.php
@@ -39,13 +39,7 @@
   <meta name="theme-color" content="#ffffff">
   
   <!-- ogp -->
-  <meta property="fb:admins" content="100006838625868">
-  <meta property="og:title" content="<?php $title = wp_get_document_title(); echo $title; ?>">
-  <meta property="og:type" content="<?php if( is_home() ) { echo 'website'; } else { echo 'article'; } ?>">
-  <meta property="og:url" content="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">
-  <meta property="og:image" content="<?php if ( is_single() && wp_get_attachment_url(get_post_thumbnail_id()) != '' ) { echo wp_get_attachment_url(get_post_thumbnail_id()); } elseif ( is_single() && wp_get_attachment_url(get_post_thumbnail_id()) == '' ) { echo get_bloginfo('template_url').'/assets/images/ecdg-no-images.jpg'; } else { echo get_bloginfo('template_url').'/assets/images/ecdg-eyecatch-default.png';} ?>">
-  <meta property="og:site_name" content="カラーミーショップデザインギャラリー">
-  <meta property="og:description" content="<?php my_description(); ?>">
+<?php get_template_part( 'inc/ogp' ); ?>
 
 <?php wp_head(); ?>
 </head>
