77fd943f39a00ce8285c5444baac5e66b7b1a77d
tatsuya azegami <theorthodoxworks@gmail.com>
2016-08-24 23:51:57 +0900

    Update about修正、画像変更

--- src/about.php
+++ src/about.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  *
  * Template Name: about
@@ -9,16 +9,16 @@ get_header(); ?>
 
 <section class="main__inner p-page">
   <div class="p-page__hold">
-  
+
     <div class="p-page__inner">
       <h3 class="p-page__ttl">About</h3>
       <div class="p-page__block">
         <p>COLOR ME SHOP DESIGN GALLERY は、<a href="http://px.a8.net/svt/ejp?a8mat=2BJERR+6CMH2Q+348+HWXLE" target="_blank">カラーミーショップ</a><img border="0" width="1" height="1" src="http://www10.a8.net/0.gif?a8mat=2BJERR+6CMH2Q+348+HWXLE" alt="">で制作されたECサイト（オンラインショップ）だけを集めたニッチなWebサイトギャラリーです。カラーミーショップのカート機能を利用して制作されたデザイン性の高いECサイトを中心に、カスタマイズやデザインの参考となるようなハイクオリティなサイトを厳選し掲載しています。</p>
-        <p>掲載するサイトには、カラーミーショップの<em>カート機能</em>を利用しているサイトを前提としており、「どこでもカラーミー」の機能や「カラーミーショップ API」を利用したもの、プラチナプランによる通常プランとは異なる仕様のものも含まれています。</p>
+        <p>掲載するサイトには、カラーミーショップの<em>カート機能</em>を利用しているサイトを前提としており、「どこでもカラーミー」の機能や「カラーミーショップ API」を利用したもの、プラチナプランによる通常プランとは異なる仕様のものなども含まれています。</p>
         <p>サイトの掲載希望や推薦も随時募集していますので、<a href="<?php echo esc_url(home_url('/contact/')); ?>">コンタクト</a>よりお気軽にご連絡ください（全て掲載されるとは限りません）。</p>
       </div>
     </div>
-  
+
     <div class="p-page__inner p-profile">
       <h4 class="p-page__ttl">Created By</h4>
       <div class="p-profile__inner">
@@ -31,7 +31,7 @@ get_header(); ?>
         </ul>
       </div>
     </div>
-  
+
   </div>
 </section>
 
