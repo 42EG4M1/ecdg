58b983625f19e9d44441fc6c4d489a912a1fd554
tatsuya azegami <theorthodoxworks@gmail.com>
2016-12-13 20:33:28 +0900

    Update meta追加

--- src/header.php
+++ src/header.php
@@ -9,6 +9,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?php my_description(); ?>">
   <meta name="keywords" content="カラーミーショップ, カラーミー, ネットショップ, オンラインショップ, EC, ギャラリー">
